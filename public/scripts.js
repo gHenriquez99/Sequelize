e965b448c89e916ee51c2fb429fe9c7c7ddec1d5
gHenriquez99 <henriquez.gus99@gmail.com>
2021-03-26 22:47:37 -0400

    table is fully functional. Gets the data through a fetch request. TODO: bulma

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,42 @@
-import sequelize from 'sequelize';
+async function handleData() {
+  const table = document.querySelector('.dataTable');
+  const headerRow = document.querySelector('.table-header-row');
 
+  const diningHallRequest = await fetch('/api/dining');
+  const diningHallData = await diningHallRequest.json();
+  const {data} = diningHallData;
+  console.log(data);
+  console.log(data.length);
+
+  const keys = Object.keys(data[0]);
+
+  keys.forEach((item) => {
+    const thElem = document.createElement('th');
+    thElem.classList.add('table-data');
+    thElem.innerText = item;
+    headerRow.append(thElem);
+  });
+
+  data.forEach((item) => {
+    const trElem = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+
+    td1.innerText = item.hall_id;
+    td2.innerText = item.hall_name;
+    td3.innerText = item.hall_address;
+
+    table.append(trElem);
+    trElem.append(td1);
+    trElem.append(td2);
+    trElem.append(td3);
+  });
+}
 
 async function windowActions() {
-  const request = await GET(/api/dining);
-  console.log(request);
+  console.log('Window has been loaded');
+  handleData();
 }
 
 window.onload = windowActions;
